415a09718debf9731e60fa8ee357655ceb888f1d
Jolayemi Malagu <58947920+kehinde4885@users.noreply.github.com>
2026-07-14 00:45:47 +0100

    Initialization

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('app')!
+
+/**
+ * create react root inside chosen html
+ * element if its empty
+ */
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(<RouterProvider router={router} />)
+}
